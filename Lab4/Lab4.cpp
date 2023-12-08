a594d25cc353f77faa5e2fe556fb54e01bfd3cd1
Yaroslav Bolokhonov <jroslavl.2005@gmail.com>
2023-12-08 12:18:46 +0300

    Update Lab4.cpp

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <functional>
+#include <ctime>
 
 
 template<typename T, typename Predicat>
@@ -12,19 +13,19 @@ int checkfile(std::ifstream& file);
 int main_menu();
 int task_menu();
 void ending(int n);
+void print_answer(int result, int sum_res);
 
-int task1(int n, std::istream& stream);
-int task1(int n, int a, int b);
-int task2(int n, std::istream& stream);
-int task2(int n, int a, int b);
+int task1(int n, std::istream& stream, int& cnt);
+int task1(int n, int a, int b, int& cnt);
+int task2(int n, std::istream& stream, int& sum_res);
+int task2(int n, int a, int b, int& sum_res);
 
-
+// 21
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
-	// test
 	int choice1;
 	do
 	{
@@ -41,8 +42,9 @@ int main()
 			{
 				validation(n, [](int x) {return x > 0; }, "Введите количество элементов последовательности");
 				std::cout << "Введите последовательность:\n";
-				int res = task1(n, std::cin);
-				std::cout << "Ответ: " << res << '\n';
+				int cnt{};
+				int res = task1(n, std::cin, cnt);
+				print_answer(res, cnt);
 			}
 			break;
 			case 2:
@@ -50,8 +52,9 @@ int main()
 				std::ifstream file("data.txt");
 				if (checkfile(file) == 1)
 				{
-					int res = task1(13, file);
-					std::cout << "Ответ: " << res << '\n';
+					int cnt{};
+					int res = task1(10, file, cnt);
+					print_answer(res, cnt);
 				}
 				else
 				{
@@ -66,10 +69,11 @@ int main()
 			default:
 			{
 				int a, b;
+				int cnt{};
 				validation(a, b, [](int x, int y) {return y >= x; }, "Введите диапазон [a, b]");
 				validation(n, [](int x) {return x > 0; }, "Введите количество элементов последовательности");
-				int res = task1(n, a, b);
-				std::cout << "Ответ: " << res << '\n';
+				int res = task1(n, a, b, cnt);
+				print_answer(res, cnt);
 			}
 			break;
 			}
@@ -86,8 +90,9 @@ int main()
 			{
 				validation(n, [](int x) {return x > 0; }, "Введите количество элементов последовательности");
 				std::cout << "Введите последовательность:\n";
-				int res = task2(n, std::cin);
-				std::cout << "Ответ: " << res << '\n';
+				int sum_res = 0;
+				int res = task2(n, std::cin, sum_res);
+				print_answer(res, sum_res);
 			}
 			break;
 			case 2:
@@ -95,8 +100,9 @@ int main()
 				std::ifstream file("data.txt");
 				if (checkfile(file) == 1)
 				{
-					int res = task2(13, file);
-					std::cout << "Ответ: " << res << '\n';
+					int sum_res = 0;
+					int res = task2(10, file, sum_res);
+					print_answer(res, sum_res);
 				}
 				else
 				{
@@ -113,8 +119,10 @@ int main()
 				int a, b;
 				validation(a, b, [](int x, int y) {return y >= x; }, "Введите диапазон [a, b]");
 				validation(n, [](int x) {return x > 0; }, "Введите количество элементов последовательности");
-				int res = task2(n, a, b);
-				std::cout << "Ответ: " << res << '\n';
+				int sum_res = 0;
+				int res = task2(n, a, b, sum_res);
+				print_answer(res, sum_res);
+				
 			}
 			break;
 			}
@@ -126,7 +134,7 @@ int main()
 		}
 		break;
 		}
-		
+
 	} while (choice1 != 3);
 }
 
@@ -139,9 +147,36 @@ void ending(int n)
 	else std::cout << n << " элемент\n";
 }
 
-int task1(int n, std::istream& stream)
+void print_answer(int result, int sum_res)
 {
-	int x{}, cnt{}, lastMin{};
+	switch (result)
+	{
+	case -1:
+	{
+		std::cout << "Отсутствует первый элемент входа\n";
+	}
+	break;
+	case 0:
+	{
+		std::cout << "Диапазон пустой\n";
+	}
+	break;
+	case 1:
+	{
+		std::cout << "Отсутствует последний элемент входа\n";
+	}
+	break;
+	default:
+	{
+		std::cout << "Ответ: " << sum_res << '\n';
+	}
+	}
+}
+
+int task1(int n, std::istream& stream, int& cnt)
+{
+	int x{}, res{}, lastMin{};
+	cnt = 0;
 	stream >> lastMin;
 	for (int i = 1; i < n; ++i)
 	{
@@ -155,15 +190,22 @@ int task1(int n, std::istream& stream)
 			if (x % 2 == 0)
 				cnt++;
 	}
-	return cnt;
+	if (!cnt)
+		res = 0;
+	else res = 2;
+	return res;
+		
+	// return 0 диапазон пустой
+	// return 2 есть ответ
 }
 
-int task1(int n, int a, int b)
+int task1(int n, int a, int b, int& cnt)
 {
 	std::cout << "Сгенерирована последовательность, содержащая ";
 	ending(n);
-	
-	int x{}, cnt{}, lastMin{};
+
+	int x{}, res{}, lastMin{};
+	cnt = 0;
 	lastMin = rand() % (b - a) + a;
 	std::cout << lastMin << ' ';
 	for (int i = 1; i < n; ++i)
@@ -180,59 +222,96 @@ int task1(int n, int a, int b)
 				cnt++;
 	}
 	std::cout << '\n';
-	return cnt;
-}
-
-int task2(int n, std::istream& stream)
-{
-	int x{}, sum{}, res{}, cnt{};
-	for (int i = 0; i < n; ++i)
-	{
-		stream >> x;
-		if (x > 0)
-			sum += x;
-		if (x == 7)
-		{
-			if (cnt > 0)
-				res = sum;
-			else
-				sum = 0;
-			cnt++;
-		}
-	}
-	if (cnt > 1)
-		res -= 7;
+	if (!cnt)
+		res = 0;
+	else res = 2;
 	return res;
 }
 
-int task2(int n, int a, int b)
+int task2(int n, std::istream& stream, int& sum_res)
 {
-	int x{}, sum{}, res{}, cnt{};
-	bool flag = false;
-	
-	
+	int x{}, sum{}, result = 2;
+	int firsti{}, lasti{};
+	sum_res = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		stream >> x;
+		if (!firsti)
+		{
+			if (x % 7 == 0)
+				firsti = i;
+		}
+		else
+		{
+			if (x % 7 == 0)
+			{
+				sum_res = sum;
+				lasti = i;
+			}
+			if (x > 0)
+				sum += x;
+		}
+	}
+	if (!firsti)
+		result = -1;
+	else
+	{
+		if (!lasti)
+			result = 1;
+		else
+		{
+			if (firsti + 1 == lasti)
+				result = 0;
+		}
+	}
+	return result;
+	// 0 пустой 
+	// 1 ласт = 0
+	// иначе 2
+}
+
+int task2(int n, int a, int b, int& sum_res)
+{
 	std::cout << "Сгенерирована последовательность, содержащая ";
 	ending(n);
-
+	
+	int x{}, sum{}, result = 2;
+	int firsti{}, lasti{};
+	sum_res = 0;
 	for (int i = 0; i < n; ++i)
 	{
 		x = rand() % (b - a) + a;
 		std::cout << x << ' ';
-		if (x > 0)
-			sum += x;
-		if (x == 7)
+		if (!firsti)
 		{
-			if (cnt > 0)
-				res = sum;
-			else
-				sum = 0;
-			cnt++;
+			if (x % 7 == 0)
+				firsti = i;
+		}
+		else
+		{
+			if (x % 7 == 0)
+			{
+				sum_res = sum;
+				lasti = i;
+			}
+			if (x > 0)
+				sum += x;
+		}
+	}
+	if (!firsti)
+		result = -1;
+	else
+	{
+		if (!lasti)
+			result = 1;
+		else
+		{
+			if (firsti + 1 == lasti)
+				result = 0;
 		}
 	}
 	std::cout << '\n';
-	if (cnt > 1)
-		res -= 7;
-	return res;
+	return result;
 }
 
 template<typename T, typename Predicat>
@@ -290,7 +369,7 @@ int main_menu()
 	std::cout << "2. Найти сумму положительных чисел, расположенных между первым и последним числами кратными 7\n";
 	std::cout << "3. Выход из программы\n";
 	std::cout << "-----------------------------------------------------\n";
-	
+
 	std::function<bool(int)> foo;
 	foo = [](int x)->bool {return x >= 1 && x <= 3; };
 	int choice;
